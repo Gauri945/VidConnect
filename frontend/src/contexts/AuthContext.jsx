ea5b3efc5bf88af1c8186d04657ef198eca58662
Gauri <cgauri915@gmail.com>
2026-03-03 12:32:25 +0530

    Fixed frontend submodule issue

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,294 @@
+// import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Paper from '@mui/material/Paper';
+// import Box from '@mui/material/Box';
+// import Grid from '@mui/material/Grid';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { AuthContext } from '../contexts/AuthContext';
+// import { Snackbar } from '@mui/material';
+
+
+
+// // TODO remove, this demo shouldn't need to reset the theme.
+
+// const defaultTheme = createTheme();
+
+// export default function Authentication() {
+
+    
+
+//     const [username, setUsername] = React.useState();
+//     const [password, setPassword] = React.useState();
+//     const [name, setName] = React.useState();
+//     const [error, setError] = React.useState();
+//     const [message, setMessage] = React.useState();
+
+
+//     const [formState, setFormState] = React.useState(0);
+
+//     const [open, setOpen] = React.useState(false)
+
+
+//     const { handleRegister, handleLogin } = React.useContext(AuthContext);
+
+//     let handleAuth = async () => {
+//         try {
+//             if (formState === 0) {
+
+//                 let result = await handleLogin(username, password)
+
+
+//             }
+//             if (formState === 1) {
+//                 let result = await handleRegister(name, username, password);
+//                 console.log(result);
+//                 setUsername("");
+//                 setMessage(result);
+//                 setOpen(true);
+//                 setError("")
+//                 setFormState(0)
+//                 setPassword("")
+//             }
+//         } catch (err) {
+
+//             console.log(err);
+//             let message = (err.response.data.message);
+//             setError(message);
+//         }
+//     }
+
+
+//     return (
+//         <ThemeProvider theme={defaultTheme}>
+//             <Grid container component="main" sx={{ height: '100vh' }}>
+//                 <CssBaseline />
+//                 <Grid
+//                     item
+//                     xs={false}
+//                     sm={4}
+//                     md={7}
+//                     sx={{
+//                         backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+//                         backgroundRepeat: 'no-repeat',
+//                         backgroundColor: (t) =>
+//                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+//                         backgroundSize: 'cover',
+//                         backgroundPosition: 'center',
+//                     }}
+//                 />
+//                 <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+//                     <Box
+//                         sx={{
+//                             my: 8,
+//                             mx: 4,
+//                             display: 'flex',
+//                             flexDirection: 'column',
+//                             alignItems: 'center',
+//                         }}
+//                     >
+//                         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+//                             <LockOutlinedIcon />
+//                         </Avatar>
+
+
+//                         <div>
+//                             <Button variant={formState === 0 ? "contained" : ""} onClick={() => { setFormState(0) }}>
+//                                 Sign In
+//                             </Button>
+//                             <Button variant={formState === 1 ? "contained" : ""} onClick={() => { setFormState(1) }}>
+//                                 Sign Up
+//                             </Button>
+//                         </div>
+
+//                         <Box component="form" noValidate sx={{ mt: 1 }}>
+//                             {formState === 1 ? <TextField
+//                                 margin="normal"
+//                                 required
+//                                 fullWidth
+//                                 id="username"
+//                                 label="Full Name"
+//                                 name="username"
+//                                 value={name}
+//                                 autoFocus
+//                                 onChange={(e) => setName(e.target.value)}
+//                             /> : <></>}
+
+//                             <TextField
+//                                 margin="normal"
+//                                 required
+//                                 fullWidth
+//                                 id="username"
+//                                 label="Username"
+//                                 name="username"
+//                                 value={username}
+//                                 autoFocus
+//                                 onChange={(e) => setUsername(e.target.value)}
+
+//                             />
+//                             <TextField
+//                                 margin="normal"
+//                                 required
+//                                 fullWidth
+//                                 name="password"
+//                                 label="Password"
+//                                 value={password}
+//                                 type="password"
+//                                 onChange={(e) => setPassword(e.target.value)}
+
+//                                 id="password"
+//                             />
+
+//                             <p style={{ color: "red" }}>{error}</p>
+
+//                             <Button
+//                                 type="button"
+//                                 fullWidth
+//                                 variant="contained"
+//                                 sx={{ mt: 3, mb: 2 }}
+//                                 onClick={handleAuth}
+//                             >
+//                                 {formState === 0 ? "Login " : "Register"}
+//                             </Button>
+
+//                         </Box>
+//                     </Box>
+//                 </Grid>
+//             </Grid>
+
+//             <Snackbar
+
+//                 open={open}
+//                 autoHideDuration={4000}
+//                 message={message}
+//             />
+
+//         </ThemeProvider>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import axios from "axios";
+import httpStatus from "http-status";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import server from "../environment";
+
+
+export const AuthContext = createContext({});
+
+const client = axios.create({
+    baseURL: `${server}/api/v1/users`
+})
+
+
+export const AuthProvider = ({ children }) => {
+
+    const authContext = useContext(AuthContext);
+
+
+    const [userData, setUserData] = useState(authContext);
+
+
+    const router = useNavigate();
+
+    const handleRegister = async (name, username, password) => {
+        try {
+            let request = await client.post("/register", {
+                name: name,
+                username: username,
+                password: password
+            })
+
+
+            if (request.status === httpStatus.CREATED) {
+                return request.data.message;
+            }
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    const handleLogin = async (username, password) => {
+        try {
+            let request = await client.post("/login", {
+                username: username,
+                password: password
+            });
+
+            console.log(username, password)
+            console.log(request.data)
+
+            if (request.status === httpStatus.OK) {
+                localStorage.setItem("token", request.data.token);
+                router("/home")
+            }
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    const getHistoryOfUser = async () => {
+        try {
+            let request = await client.get("/get_all_activity", {
+                params: {
+                    token: localStorage.getItem("token")
+                }
+            });
+            return request.data
+        } catch
+         (err) {
+            throw err;
+        }
+    }
+
+    const addToUserHistory = async (meetingCode) => {
+        try {
+            let request = await client.post("/add_to_activity", {
+                token: localStorage.getItem("token"),
+                meeting_code: meetingCode
+            });
+            return request
+        } catch (e) {
+            throw e;
+        }
+    }
+
+
+    const data = {
+        userData, setUserData, addToUserHistory, getHistoryOfUser, handleRegister, handleLogin
+    }
+
+    return (
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
